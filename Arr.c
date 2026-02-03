0f5800bacbff0494b39b0802f0b5039d9e7fc30b
Jose Emilio Antonio Jimenez <95834107+Altem1@users.noreply.github.com>
2026-02-03 17:46:04 +0000

    Apuntadores con Struct

---
+++ Arr.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    
+    int clave;
+    char *nombre;
+    char *carrera;
+    int generacion;
+
+}TAlumno;
+
+void crea_arregloAlumnos1D(TAlumno **ptrA, int na){
+
+    *ptrA=(TAlumno*) malloc (sizeof(TAlumno)*na);
+    if (*ptrA==NULL){
+        printf("No");
+    }
+
+}
+
+void captura_alumnoS(TAlumno *ptrA, int na){
+
+    char cadena[100];
+
+    for (int i = 0; i < na; i++){
+        printf("Clave: ");
+        scanf("%d", &(ptrA+i)->clave);
+
+        printf("Nombre: ");
+        scanf("%[^\n], cadena");
+        (ptrA+i)->nombre=(char*)malloc(sizeof(char)*(strlen(cadena)+1));
+        strcp((ptrA+i)->nombre, cadena);
+
+        printf("Carrera: ");
+        scanf("%[^\n], cadena");
+        (ptrA+i)->carrera=(char*)malloc(sizeof(char)*strlen(cadena+1));
+        strcp((ptrA+i)->carrera, cadena);
+
+        printf("Generacion: ");
+        scanf("%d", &(ptrA+i)->generacion);
+    }
+
+}
