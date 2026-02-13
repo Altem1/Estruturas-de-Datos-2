5f1f35e9e65af453d0b7eb33c774c0d720419299
Altem1 <ja8406685@gmail.com>
2026-02-13 08:57:20 -0600

    ningun cambio

--- Unidad_1/ABCAlumnos.c
+++ Unidad_1/ABCAlumnos.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void altas(TAlumno *arr, int na);
-void bajas(TAlumno *arr, int *na, int clave1);
-
 typedef struct{
     
     int clave;
@@ -14,39 +11,89 @@ typedef struct{
 
 }TAlumno;
 
+int menu();
+void altas(TAlumno **arr, int *na);
+void bajas(TAlumno *arr, int *na, int clave1);
+void captura_alumno(TAlumno *ptrA);
+void mostrar_alumnos(TAlumno *arr);
+TAlumno *busquedas(TAlumno *arr, int clave, int na);
+void liberar_todo(TAlumno *arr, int na);
+
+int main(){
+
+    int op, clavee;
+    TAlumno *arr=NULL, *busco;
+    int na=0;
+
+    do{
+        op=menu();
+        switch(op){
+            case 1:
+                printf("\nAgrega los datos: \n");
+                altas(&arr, &na);
+                break;
+            case 2:
+                printf("\nIndica la clave del alumno: \n");
+                scanf("%d", &clavee);
+                bajas(arr, &na, clavee);
+                break;
+            case 3:
+                printf("\nIndica la clave del alumno: ");
+                scanf("%d", &clavee);
+                busco=busquedas(arr, clavee, na);
+                if(busco==NULL){
+                    printf("Error NULL");
+                    break;
+                }
+                mostrar_alumnos(busco);
+                break;
+            case 4:
+                printf("\nLista de alumnos registrados.. \n");
+                for(int i=0; i<na; i++){
+                    printf("\nDatos del alumno %d: \n", i+1);
+                    mostrar_alumnos(arr+i);
+                }
+                break;
+            
+            default:
+                printf("\nSaliendo...\n");
+                liberar_todo(arr, na);
+                break;
+
+            
+        }
+    }while(op<5);
+
+
+}
+
 int menu(){
 
     int opcion;
-    printf("1. Altas de alumnos\n");
+    printf("\n1. Altas de alumnos\n");
     printf("2. Bajas de alumnos\n");
-    printf("3. Modificaciones de alumnos\n");
-    printf("4. Consulta de alumnos\n");
-    printf("5. Impresion de alumnos\n");
-    printf("6. Salir\n");
+    printf("3. Consulta de alumno\n");
+    printf("4. Impresion de alumnos\n");
+    printf("5. Salir\n");
     printf("Selecciona una opcion: ");
     scanf("%d", &opcion);
     return opcion;
 
 }
 
-void altas(TAlumno *arr, int na){
+void altas(TAlumno **arr, int *na){
 
-    *arr=(TAlumno*)realloc(TAlumno*)*arr,sizeof(TAlumno)*na);
+    *arr=(TAlumno*)realloc(*arr,sizeof(TAlumno)*(*na + 1));
     if(*arr==NULL){
         printf("Error al asignar memoria");
         return;
     }
-    captura_alumno(arr);
-
-}
-
-void consulta(){
-
-
+    captura_alumno(*arr + *na);
+    (*na)++;
 }
 
 //necesito buscar al alumno
-TAlumno *busqueda(TAlumno *arr, int clave, int na){
+TAlumno *busquedas(TAlumno *arr, int clave, int na){
 
     TAlumno *encontrado=NULL;
 
@@ -64,37 +111,57 @@ TAlumno *busqueda(TAlumno *arr, int clave, int na){
 void bajas(TAlumno *arr, int *na, int clave1){
 
     TAlumno *buscar=NULL;
-    buscar=busqueda(arr, clave1, *na);
+    buscar=busquedas(arr, clave1, *na);
     if(buscar){
         //Se encontro el alumno
         *buscar = *(arr + (*na - 1));
-        (*na)--1;
+        (*na)--;
     }
 
 }
 
-void captura_alumnoS(TAlumno *ptrA){
+void captura_alumno(TAlumno *ptrA){
 
     char cadena[100];
 
         printf("Clave: ");
-        scanf("%d", &(ptrA)->clave);
+        scanf("%d", &ptrA->clave);
+        getchar();
 
         printf("Nombre: ");
-        scanf("%[^\n], cadena");
-        (ptrA+i)->nombre=(char*)malloc(sizeof(char)*(strlen(cadena)+1));
-        strcp((ptrA+i)->nombre, cadena);
+        scanf(" %[^\n]", cadena);
+        (ptrA)->nombre=(char*)malloc(sizeof(char)*(strlen(cadena)+1));
+        strcpy((ptrA)->nombre, cadena);
+        getchar();
 
         printf("Carrera: ");
-        scanf("%[^\n], cadena");
-        (ptrA+i)->carrera=(char*)malloc(sizeof(char)*strlen(cadena+1));
-        strcp((ptrA+i)->carrera, cadena);
+        scanf(" %[^\n]", cadena);
+        (ptrA)->carrera=(char*)malloc(sizeof(char)*strlen(cadena+1));
+        strcpy((ptrA)->carrera, cadena);
 
         printf("Generacion: ");
-        scanf("%d", &(ptrA+i)->generacion);
+        scanf("%d", &ptrA->generacion);
 
 }
 
-void imprimir(TAlumnos *arr, int *na){
+void mostrar_alumnos(TAlumno *arr){
+    
+        printf("\nClave: %d", ((arr)->clave));
+        printf("\nNombre:  %s", ((arr)->nombre));
+        printf("\nCarrera:  %s", ((arr)->carrera));
+        printf("\nGeneracion: %d", ((arr)->generacion));
+    
+}
+
+void liberar_todo(TAlumno *arr, int na){
+
+    for(int i=0; i<na; i++){
+
+        free((arr+i)->nombre);
+        free((arr+i)->carrera);
+
+    }
+
+    free(arr);
 
 }
