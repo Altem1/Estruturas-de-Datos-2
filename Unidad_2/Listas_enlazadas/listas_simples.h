6b0ac94c1be395b61b300a4e5711ee7a7e26b324
Altem1 <ja8406685@gmail.com>
2026-03-11 19:55:32 -0600

    Se creo la carpeta para la tarea 4 de Listas enlazadas la cual tiene como objetivo modificar las funciones fundamentales para poder realizar un programa que pueda capturar una ecuacion donde tendra coeficiente y exponente

---
+++ Unidad_2/Listas_enlazadas/listas_simples.h
@@ -0,0 +1,28 @@
+#ifndef LISTAS_SIMPLES_H
+#define LISTAS_SIMPLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int tipo;
+
+typedef struct nodo{
+	tipo info;
+	struct nodo *sig;
+}TNodo;
+
+//Listas Simples
+TNodo *crea_nodo(tipo d);
+void imprime_lista(TNodo *cab);
+void inserta_inicio(TNodo **cab, tipo d);
+void inserta_final(TNodo **c, tipo d);
+void inserta_finalR(TNodo **c, tipo d);
+void inserta_ordenado(TNodo **C, tipo d);
+void inserta_ordenadoR(TNodo **C, tipo d);
+void elimina_inicio(TNodo **c);
+tipo elimina_final(TNodo **c);
+tipo elimina_finalR(TNodo **c);
+int elimina_x(TNodo **cab, tipo x);   
+int elimina_xR(TNodo **cab, tipo x);
+
+#endif
